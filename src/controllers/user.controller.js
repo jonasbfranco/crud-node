240aee5e484fa65d53c0f9d0b9da46132b1246d4
Jonas B. Franco <jonasbfranco@hotmail.com>
2024-06-18 19:36:20 -0300

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import { userValidation } from "../validations/user.validation";
-import { createUser, getAll, getById } from "../repositorys/user.repository";
+import { createUser, getAll, getById, updateUser, deleteUser } from "../repositorys/user.repository";
 
 export const create = async (req, res) => {
     try {
@@ -35,3 +35,25 @@ export const getId = async (req, res) => {
     }
 };
 
+
+export const update = async (req, res) => {
+    try {
+        // await userValidation.validate(req.body);
+
+        // const hashPassword = await bcrypt.hash(req.body.password, 10);
+        // req.body.password =  hashPassword;
+        const user = await updateUser(Number(req.params.id), req.body);
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+export const remove = async (req, res) => {
+    try {
+        await deleteUser(Number(req.params.id));
+        res.status(200).send();
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
